cd8b4c8defbe1fa26111da3684722fbd1bb64a4a
Man Hong Lee <manhong323@gmail.com>
2017-10-18 18:26:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {fruit : 'apple'};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, [key : value]);
+  return Object.assign({}, object, {[key] : value});
 }
